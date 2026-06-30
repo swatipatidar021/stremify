827b99428a8b0bef240464d523dd467f4b7a84f4
unknowntech2341 <satya.blbhilala786@gmail.com>
2026-06-30 19:32:32 +0530

    update cors for vercel

--- streamify-video-calls-master/frontend/src/lib/axios.js
+++ streamify-video-calls-master/frontend/src/lib/axios.js
@@ -1,8 +1,15 @@
+// import axios from "axios";
+
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
+// export const axiosInstance = axios.create({
+//   baseURL: BASE_URL,
+//   withCredentials: true, // send cookies with the request
+// });
+
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
-
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true, // send cookies with the request
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
