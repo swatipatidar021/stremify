268dc22cb9e8b53317d83fe354d0a81dc8570d6e
unknowntech2341 <satya.blbhilala786@gmail.com>
2026-06-30 19:50:25 +0530

    update cors for vercel

--- streamify-video-calls-master/backend/src/controllers/auth.controller.js
+++ streamify-video-calls-master/backend/src/controllers/auth.controller.js
@@ -53,7 +53,7 @@ const randomAvatar = `https://api.dicebear.com/7.x/bottts/svg?seed=${idx}`;
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
+      sameSite: "none", // prevent CSRF attacks
       secure: process.env.NODE_ENV === "production",
     });
 
@@ -85,7 +85,7 @@ export async function login(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
+      sameSite: "none", // prevent CSRF attacks
       secure: process.env.NODE_ENV === "production",
     });
 
